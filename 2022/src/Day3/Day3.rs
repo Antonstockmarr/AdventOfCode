706f75ce12ac70b5c4e5b1fe8d4fed64edce4a84
Antonstockmarr <43210147+Antonstockmarr@users.noreply.github.com>
2022-12-03 10:11:52 +0100

    Day 3
    rust

---
+++ 2022/src/Day3/Day3.rs
@@ -0,0 +1,73 @@
+use std::fs;
+use std::iter::Peekable;
+
+fn priority(letter : char) -> i32 {
+    let ascii = letter as i32;
+    println!("{}", ascii);
+    if ascii < 91 {
+        return ascii - 64 + 26;
+    }
+    else {
+        return ascii - 96
+    }
+}
+
+fn calculate_part_one_sum(lines: &mut Peekable<std::str::Lines<'_>>) {
+    let mut sum = 0;
+    while lines.peek().is_some() {
+        let current = lines.next().unwrap();
+        println!("current {}", current);
+        
+        let letters : Vec<char> = current.chars().collect();
+        let n = letters.len();
+        'outer: for i in 0..(n/2) {
+            for j in (n/2)..n {
+                if letters[i] == letters[j] {
+                    println!("{}",letters[i]);
+                    sum += priority(letters[i]);
+                    println!("{}",priority(letters[i]));
+                    break 'outer;
+                }
+            }
+        }
+    }
+    println!("Sum {:?}", sum);
+}
+
+fn calculate_part_two_sum(lines: &mut Peekable<std::str::Lines<'_>>) {
+    let mut sum = 0;
+    while lines.peek().is_some() {
+        let first_line = lines.next().unwrap();
+        let second_line = lines.next().unwrap();
+        let third_line = lines.next().unwrap();
+        
+        let first_letters : Vec<char> = first_line.chars().collect();
+        let second_letters : Vec<char> = second_line.chars().collect();
+        let third_letters : Vec<char> = third_line.chars().collect();
+        'outer: for a in first_letters.iter() {
+            for b in second_letters.iter() {
+                if *a == *b {
+                    for c in third_letters.iter() {
+                        if *a == *c {
+                            println!("{}",*a);
+                            sum += priority(*a);
+                            break 'outer;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("Sum {:?}", sum);
+}
+
+fn main() {
+    let file_path = "../../data/Day3.txt";
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+
+    let mut lines = contents.lines().peekable();
+    //calculate_part_one_sum(&mut lines);
+    calculate_part_two_sum(&mut lines);
+}
